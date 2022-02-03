cdeb39048abf991fb30e0c8a59acfca45e50719b
Kitsanapong Warit <kitsanapong.warit@hotmail.com>
2022-02-04 00:23:38 +0700

    Initial commit

---
+++ src/com/company/MainSLL.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class MainSLL {
+    public static void main(String[] args) {
+        SLL sll = new SLL();
+        sll.addFirst(2);
+        sll.addLast(3);
+        sll.addFirst(1);
+        sll.print();
+        sll.removeFirst();
+        sll.print();
+        sll.removeLast();
+        sll.print();
+    }
+}
+
---
+++ src/com/company/SLL.java
@@ -0,0 +1,138 @@
+package com.company;
+
+class SLL {
+    // ----------------------- Node --------------------
+    private class Node {
+        private int element;
+        private Node next;
+
+        // constructor
+        public Node(int data) {
+            element = data;
+            next = null;
+        }
+
+        // link a new node to this node
+        public void link(Node newNode) {
+            next = newNode;
+        }
+
+        // return next node
+        public Node getNextNode() {
+            return next;
+        }
+
+        // return element of this node
+        public int getElement() {
+            return element;
+        }
+    }
+    // --------------------- End Node --------------------
+
+    // SLL properties and methods
+    private Node head = null;
+    private Node tail = null;
+    private int size = 0; // SLL's size
+
+    public void addLast(int data) {
+        // create new node
+        Node newNode = new Node(data);
+        if (size == 0) {
+            head = newNode;
+//            tail = newNode;
+//            size++;
+        }
+        else {
+            tail.link(newNode);
+        //    tail = newNode;
+        //    size++;
+
+        }
+            tail = newNode;
+            size++;
+    }
+
+    public void addFirst(int data) {
+
+        Node newNode = new Node(data);
+
+        if (size == 0)
+        {
+            tail = newNode;
+//            head = newNode;
+//            size++;
+        }
+
+        else
+        {
+            newNode.link(head);
+        }
+            head=newNode;
+            size++;
+
+
+
+    }
+
+    public void removeFirst() {
+
+//        if(size == 0)
+//        {
+//
+//        }
+
+        if(size == 1)
+        {
+            head = null;
+            tail = null;
+
+        }
+
+        else
+        {
+            head=head.getNextNode();
+        }
+
+        size--;
+
+    }
+
+    public void removeLast() {
+
+        if(size == 1)
+        {
+            head = null;
+            tail = null;
+            size--;
+        }
+
+        else if(size > 1)
+        {
+            Node pointer = head;
+
+            while (pointer.getNextNode() != tail)
+            {
+                pointer = pointer.getNextNode();
+            }
+
+            tail = pointer;
+            tail.link(null);
+            size--;
+        }
+
+
+
+    }
+
+    public void print() {
+        if (size == 0) {
+            System.out.println("Empty linked list");
+        } else {
+            for (Node pointer = head; pointer != null; pointer = pointer.getNextNode()) {
+                System.out.print(pointer.getElement() + "->");
+            }
+            System.out.println("null");
+        }
+    }
+}
+
